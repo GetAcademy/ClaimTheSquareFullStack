96630556009c1859f0c857814576490757866586
GET Academy <terje@getacademy.no>
2024-01-02 10:59:30 +0100

    x

--- ClaimTheSquareFullStack/Program.cs
+++ ClaimTheSquareFullStack/Program.cs
@@ -1,13 +1,18 @@
 using ClaimTheSquareFullStack.ViewModel;
 
+// Pause til 11:05
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseHttpsRedirection();
+var textObjects = new List<TextObject>();
 app.MapGet("/textobjects", () =>
 {
-    return new TextObject[]{
-        new TextObject(17, "Terje", "green", "white")
-    };
+    return textObjects;
+});
+app.MapPost("/textobjects", (TextObject textObject) =>
+{
+    textObjects.Add(textObject);
 });
 app.UseStaticFiles();
 app.Run();
